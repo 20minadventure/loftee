58fe7af84b7dd6c092abd1623383c8d5354776e8
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-02-23 12:27:45 +0000

    blocks

--- analysis/split_vep.py
+++ analysis/split_vep.py
@@ -20,3 +20,25 @@ def main():
 
 
     hl_init(tmp_dir=tmp_path.rstr, log=log_path)
+
+    b_vcf = PathDx('/mnt/project/Bulk/Exome sequences/Population level exome OQFE variants, pVCF format - final release')
+    for p in b_vcf.listdir():
+        m = re.fullmatch(r'ukb23157_c(\d{1,2}|X|Y)_b(\d{1,3})_v1.vcf.gz', p.name)
+        if m:
+            contig = m.group(1)
+            block = m.group(2)
+            chr_b_path = tmp_path / f'chr-{contig}-b{block}.mt'
+            if contig == '13' and chr_b_path not in tmp_path.listdir():
+                print(chr_b_path)
+                try:
+                    mt = hl.import_vcf(
+                        p.rstr,
+                        force_bgz=True,
+                        array_elements_required=False,
+                    )
+                    mt = hl.split_multi_hts(mt)
+                    mt = hl.vep(mt, vep_config_path.rstr)
+
+                    mt.write(chr_b_path.rstr)
+                except Exception:
+                    print('ERROR: ', p)
