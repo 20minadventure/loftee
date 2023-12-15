c979530e7f2cce703025b260e63c1b062aea6cbc
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-12-15 20:08:31 +0100

    Use variable for temporary hail database name

--- analysis/split_vep.py
+++ analysis/split_vep.py
@@ -35,7 +35,7 @@ def main():
 
     db_hail_tmp = 'hail_tmp_bl'
     SC.sql(f"CREATE DATABASE IF NOT EXISTS {db_hail_tmp} LOCATION 'dnax://'")
-    hail_tmp_path = PathDx(database='hail_tmp_bl')
+    hail_tmp_path = PathDx(database=db_hail_tmp)
 
     log_path = f'/tmp/{datetime.now().strftime("%Y%m%d-%H%M")}-{random.randrange(16 ** 6):04x}.log'
 
