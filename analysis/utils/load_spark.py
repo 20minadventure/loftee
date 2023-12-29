76bfd26da2d3075d197eae4895cde3f7a78dbed5
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-12-29 04:23:21 +0100

    Reduce spark driver and executor memory

--- analysis/utils/load_spark.py
+++ analysis/utils/load_spark.py
@@ -15,8 +15,8 @@ if 'aws' in platform.release():
     builder = (
         SparkSession
         .builder
-        .config('spark.driver.memory', '120G')
-        .config('spark.executor.memory', '120G')
+        .config('spark.driver.memory', '60G')
+        .config('spark.executor.memory', '60G')
         .config('spark.ui.showConsoleProgress', 'false')
         .enableHiveSupport()
     )
